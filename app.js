e18e0a66752d3da71b286d986076e40fd0b62885
mithil <mghinaiya@gmail.com>
2021-07-05 23:22:34 +0530

    added dependencies, updated docs

--- app.js
+++ app.js
@@ -3,9 +3,12 @@
     , ini = require('ini');
     const cp = require("child_process");
     var dgram = require('dgram');
-const { request } = require('http');
+    var ip = require("ip");
     var ip_addr ;
     var ip_list= [];
+    var PORT = 33333;
+    var dgram = require('dgram');
+    var client = dgram.createSocket('udp4');
 
 function config_stel(){
 
@@ -36,42 +39,62 @@ function config_stel(){
 
 function launch_stel(){
     cp.exec("stellarium"); // notice this without a callback..
+    var HOST = ip_addr;
+    if (this_pc== 1){
+        
+        var message = new Buffer('Launch Stellarium');
+        
+        //var message= "Launch Stellarium";
+        for (let i=0; i<ip_list.length; i++){
+            client.send(message, 0, message.length, PORT, ip_list[i], function(err, bytes) {
+                if (err) throw err;
+                console.log('UDP message sent to ' + ip_list[i] +':'+ PORT);
+                client.close();
+            });
+        }
+    }
     //process.exit(0); // exit this nodejs process
 
 }
-var ip = require("ip");
-console.dir ( ip.address() );
+
 
 function socket_server(){
-    var PORT = 33333;
+    // var PORT = 33333;
     var HOST =   ip.address();
     console.log("in server fn");
     
     var server = dgram.createSocket('udp4');
     server.on('listening', function() {
-    var address = server.address();
-    console.log('UDP Server listening on ' + address.address + ':' + address.port);
+        var address = server.address();
+        console.log('UDP Server listening on ' + address.address + ':' + address.port);
+        //ip_list.push(address.address);
     });
-
-    server.on('message', function(message, remote) {
-    console.log(remote.address + ':' + remote.port +' - ' + message);
-    ip_list.push(remote.address)
-    });
+    var launched= false;
+    while(launched== false){
+        server.on('message', function(message, remote) {
+            console.log(remote.address + ':' + remote.port +' - ' + message);
+            if (message== "Launch Stellarium") {
+                cp.exec("stellarium");
+                launched= true;
+            }
+            ip_list.push(remote.address)
+        });
+    }
 
     server.bind(PORT, HOST);
 }
 
 function socket_client(){
-    var PORT = 33333;
+    
     var HOST = ip_addr;
     console.log("in client fn");
-    var dgram = require('dgram');
     var message = new Buffer('My KungFu is Good!');
 
-    var client = dgram.createSocket('udp4');
     client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
         if (err) throw err;
         console.log('UDP message sent to ' + HOST +':'+ PORT);
         client.close();
     });
+    socket_server();
+    
 }
